c8e43d8bed87d64bd4dd7bab81b71739ebba4ac8
P Roudi <52068944+jiaoao0305@users.noreply.github.com>
2023-06-02 17:36:25 -0700

    removed unnecessary depndency from viz_fun2.py. Deleted lina's key

--- Dashboardfunctions/lina_viz2.py
+++ Dashboardfunctions/lina_viz2.py
@@ -1,17 +1,15 @@
-import requests
-# from googleapiclient.discovery import build
 import pandas as pd
 import plotly.express as px
 import re
 from collections import Counter
-# import plotly.graph_objects as go
-api_key = 'AIzaSyDSz4TNw2LDHmdotHZoZb90Oe06z8FOC2Y'
-# "AIzaSyBqgqP0nQ-qP4LJc4_kytQTERSq-pXOff0" # can change it to your own api 
-###############################################################################    
+
+###############################################################################
+
+
 def plot_most_recent(df, column):
     """
     plots the specified statistics of the most recent 10 Youtube channel's videos:
-    
+
     @df: a channel specific data frame
     @column: a string specifying the column to define top 10; 
             4 options: 'Like Count', 'View Count', 'Comment Count', 'Duration'
@@ -27,42 +25,42 @@ def plot_most_recent(df, column):
         col = 'Comments'
     elif column == 'Duration':
         df_column = 'duration(min)'
-        
+
     # youtube = build("youtube","v3", developerKey=api_key)
     # in_stats_df = channels_stats(youtube,[user])
     # video_ids = get_videoID_list(youtube,user)
     # video_stats = get_video_details(youtube,video_ids)
     # most_recent10 = video_stats.sort_values("published", ascending = False).head(10)
     # most_recent10[df_column] = most_recent10.loc[:,df_column].astype(int)
-    most_recent10 = df.sort_values('published',ascending=False).head(10)
-    most_recent10[df_column] = most_recent10.loc[:,df_column].astype(float)
-    most_recent10['published'] = pd.to_datetime(most_recent10['published'])# .dt.date
+    most_recent10 = df.sort_values('published', ascending=False).head(10)
+    most_recent10[df_column] = most_recent10.loc[:, df_column].astype(float)
+    most_recent10['published'] = pd.to_datetime(
+        most_recent10['published'])  # .dt.date
 
-    fig = px.line(most_recent10, x = 'published', y = df_column,
-            hover_name = 'title', markers=True,
-            hover_data = {'title':False, 'published':True, df_column:True}, color_discrete_sequence=['#d62728'])
-    
+    fig = px.line(most_recent10, x='published', y=df_column,
+                  hover_name='title', markers=True,
+                  hover_data={'title': False, 'published': True, df_column: True}, color_discrete_sequence=['#d62728'])
+
     fig.update_xaxes(fixedrange=True)
-    
+
     if column == 'Duration':
-        fig.update_layout(title = 'Duration in Minutes of 10 Most Recent Videos',
-                          xaxis_title = 'Date Published', 
-                          yaxis_title= 'Duration in Minutes')
+        fig.update_layout(title='Duration in Minutes of 10 Most Recent Videos',
+                          xaxis_title='Date Published',
+                          yaxis_title='Duration in Minutes')
     else:
-        fig.update_layout(title = 'Number of ' + col + ' on 10 Most Recent Videos',
-                          xaxis_title = 'Date Published', 
-                          yaxis_title= 'Number of ' + col)
-        
+        fig.update_layout(title='Number of ' + col + ' on 10 Most Recent Videos',
+                          xaxis_title='Date Published',
+                          yaxis_title='Number of ' + col)
+
     # fig = go.Figure()
     # fig.add_trace(go.Bar(x = df['tag'], y = df['count']))
     # fig.add_trace(go.Scatter(x = df['tag'], y = df['count']))
-    
-    
+
     # fig.update_layout(
     #     xaxis = dict(
     #         tickmode = 'array',
     #         tickvals = list(range(0,10)),
-    #         ticktext = [most_recent10.title[0][:len(most_recent10.title[0])//2] + '...', 
+    #         ticktext = [most_recent10.title[0][:len(most_recent10.title[0])//2] + '...',
     #                     most_recent10.title[1][:len(most_recent10.title[1])//2] + '...',
     #                     most_recent10.title[2][:len(most_recent10.title[2])//2] + '...',
     #                     most_recent10.title[3][:len(most_recent10.title[3])//2] + '...',
@@ -74,7 +72,7 @@ def plot_most_recent(df, column):
     #                     most_recent10.title[9][:len(most_recent10.title[9])//2] + '...',]
     #     )
     # )
-    
+
     # fig.update_layout(
     #     xaxis = dict(
     #         tickmode = 'array',
@@ -82,52 +80,57 @@ def plot_most_recent(df, column):
     #         ticktext = ['1','2','3','4','5','6','7','8','9','10']
     #     )
     # )
-    
+
     fig.update_layout(hovermode="x")
     fig.update_layout(autosize=False,
                       width=1000,
                       height=500,
                       margin=dict(l=100, r=100, t=100, b=100))
     return fig
-###############################################################################    
+###############################################################################
+
+
 def plot_top_tags(df):
     """
     plots the top 5 tags used of a specified channel:
-    
+
     @df: a channel specific data frame
     """
-    
+
     # youtube = build("youtube","v3", developerKey=api_key)
     # in_stats_df = channels_stats(youtube,[user])
     # video_ids = get_videoID_list(youtube,user)
     # video_stats = get_video_details(youtube,video_ids)
     # top_tags = top_tags_modified(list(video_stats.tags))
     top_tags = top_tags_modified(list(df.tags))
-    
+
     tags = [tag for tag, _ in top_tags]
     counts = [count for _, count in top_tags]
-    df = pd.DataFrame({'tag' : tags, 'count' : counts})
-    
+    df = pd.DataFrame({'tag': tags, 'count': counts})
+
     # fig = go.Figure()
     # fig.add_trace(go.Bar(x = df['tag'], y = df['count']))
     # fig.add_trace(go.Scatter(x = df['tag'], y = df['count']))
-    
-    fig = px.bar(df, x = 'tag', y = 'count', hover_name = 'tag', 
-                 hover_data = {'tag':False, 'count':True}, color_discrete_sequence=['#d62728'])
-    
+
+    fig = px.bar(df, x='tag', y='count', hover_name='tag',
+                 hover_data={'tag': False, 'count': True}, color_discrete_sequence=['#d62728'])
+
     fig.update_layout(hovermode="x")
-    fig.update_layout(title = 'Overall Top 5 Most Used Tags', xaxis_title = 'Tag', yaxis_title= 'Number of Times Used', showlegend=False)
-    
+    fig.update_layout(title='Overall Top 5 Most Used Tags', xaxis_title='Tag',
+                      yaxis_title='Number of Times Used', showlegend=False)
+
     fig.update_layout(autosize=False,
                       width=1000,
                       height=500,
                       margin=dict(l=100, r=100, t=100, b=100))
     return fig
-#########################helper functions#######################################    
+######################### helper functions#######################################
+
+
 def top_tags_modified(tags_list):
     """
     MODIFIED helper function for plot_top_tags and returns the top 5 tags and their number of occurrences for a specific channel
-    
+
     @tags_list: a list of lists of tags for all the tags used by a specific channel
     """
     all_strings = [string for sublist in tags_list for string in sublist]
@@ -137,17 +140,19 @@ def top_tags_modified(tags_list):
     else:
         top_tags = string_counts.most_common(len(string_counts))
     return top_tags
-############################################################################### 
+###############################################################################
+
+
 def top_tags(tags_list):
     """
     helper function for plot_top_tags and returns the top 5 tags and their number of occurrences for a specific channel
-    
+
     splits each list nested within the lists items into individual words
-    
+
     @tags_list: a list of lists of words for all the tags used by a specific channel
     """
     word_pattern = re.compile(r'\b\w+\b')  # Matches individual words
-    
+
     all_words = []
     for sublist in tags_list:
         all_words.extend(word_pattern.findall(' '.join(sublist)))
@@ -157,10 +162,6 @@ def top_tags(tags_list):
         top_tags = word_counts.most_common(5)
     else:
         top_tags = word_counts.most_common(len(all_words))
-    
-    return top_tags
-###############################################################################    
-   
 
-    
-    
+    return top_tags
+###############################################################################
--- Dashboardfunctions/metrics_samviz.py
+++ Dashboardfunctions/metrics_samviz.py
@@ -1,10 +1,11 @@
-import numpy as np
 import pandas as pd
 import plotly.express as px
 from Dashboardfunctions.api_fun import *
 import streamlit as st
 
-##Overall Metrics
+# Overall Metrics
+
+
 def avg_views(df):
     '''
     Returns average views. Works for both pages of dashboard
@@ -25,7 +26,8 @@ def top_topics(topic_df):
     Returns top 10 most popular topics. Only works for page 1
     '''
     st.write(topic_df)
-    return topic_df.nlargest(20,["Number of Channels"])[0:10]["Topic"]
+    return topic_df.nlargest(20, ["Number of Channels"])[0:10]["Topic"]
+
 
 def avg_duration(one_channel_df):
     '''
@@ -33,93 +35,83 @@ def avg_duration(one_channel_df):
     '''
     return round(one_channel_df["duration(min)"].mean())
 
+
 def top_vid_avg_duration(one_channel_df):
     '''
     Average length of top 20 videos based on views
     '''
-    x = one_channel_df.nlargest(20,["viewCount"])
+    x = one_channel_df.nlargest(20, ["viewCount"])
     return round(x["duration(min)"].mean())
 
 
-
-
-
-##Topic visualizations
+# Topic visualizations
 # Needs to run topic_df = top_topics(homepage_df)
-#----------------------
+# ----------------------
 def topic_count_viz_sam(topic_df):
     '''
     Shows number of channels per topic
     '''
-    topic_df = topic_df.nlargest(20,["Average Subscriber Count"])
-    fig = px.bar(topic_df, y= "Topic", x = "Number of Channels", title = "Top 20 Topics based on Appearance on Youtube Trending Page",color = "Number of Channels",orientation = "h", color_continuous_scale = "Agsunset" )
-    fig.update_layout(yaxis={'categoryorder':'total ascending'})
+    topic_df = topic_df.nlargest(20, ["Average Subscriber Count"])
+    fig = px.bar(topic_df, y="Topic", x="Number of Channels", title="Top 20 Topics based on Appearance on Youtube Trending Page",
+                 color="Number of Channels", orientation="h", color_continuous_scale="Agsunset")
+    fig.update_layout(yaxis={'categoryorder': 'total ascending'})
     return fig
-    
-    
+
+
 def topic_avg_sub_viz_sam(topic_df):
     '''
     Shows number of channels per topic
     '''
-    topic_df = topic_df.nlargest(20,["Average Subscriber Count"])
-    fig = px.bar(topic_df, y= "Topic", x = "Average Subscriber Count", title = "Top 20 Topics Based on Average Number of Subscribers",color = "Average Subscriber Count",orientation = "h", color_continuous_scale = "Darkmint" )
-    fig.update_layout(yaxis={'categoryorder':'total ascending'})
+    topic_df = topic_df.nlargest(20, ["Average Subscriber Count"])
+    fig = px.bar(topic_df, y="Topic", x="Average Subscriber Count", title="Top 20 Topics Based on Average Number of Subscribers",
+                 color="Average Subscriber Count", orientation="h", color_continuous_scale="Darkmint")
+    fig.update_layout(yaxis={'categoryorder': 'total ascending'})
     return fig
-    
-    
-    
-    
+
+
 def topic_avg_view_viz_sam(topic_df):
     '''
     Shows number of channels per topic
     '''
-    topic_df = topic_df.nlargest(20,["Average Channel View Count"])
-    fig = px.bar(topic_df, y= "Topic", x = "Average Channel View Count", title = "Top 20 Topics Based on Average Number of Views",color = "Average Channel View Count",orientation = "h", color_continuous_scale = "Viridis" )
-    fig.update_layout(yaxis={'categoryorder':'total ascending'})
+    topic_df = topic_df.nlargest(20, ["Average Channel View Count"])
+    fig = px.bar(topic_df, y="Topic", x="Average Channel View Count", title="Top 20 Topics Based on Average Number of Views",
+                 color="Average Channel View Count", orientation="h", color_continuous_scale="Viridis")
+    fig.update_layout(yaxis={'categoryorder': 'total ascending'})
     return fig
-    
 
 
-
-    
-    
-    
-    
-    
-      
-
-
-##Helper Functions
-#----------------------
+# Helper Functions
+# ----------------------
 
 def topics_df(homepage_df):
     '''
     returns a dataframe with "Topic","Number of Channels","Average Subscriber Count","Average Channel View Count" column. 
-    
+
     '''
     index = []
     for i in homepage_df["topic"]:
         x = i is not None
-        index.append(x) 
+        index.append(x)
     topic_counts = homepage_df["topic"][index].explode().value_counts()
-    topic_df = pd.DataFrame({"Topic" : topic_counts.index, "Number of Channels" :topic_counts.values })
-    
-    topic_df["Average Subscriber Count"] = topic_df["Topic"].map(topic_averages(homepage_df,"subscriberCount"))
-    topic_df["Average Channel View Count"] = topic_df["Topic"].map(topic_averages(homepage_df,"viewCount"))
+    topic_df = pd.DataFrame(
+        {"Topic": topic_counts.index, "Number of Channels": topic_counts.values})
+
+    topic_df["Average Subscriber Count"] = topic_df["Topic"].map(
+        topic_averages(homepage_df, "subscriberCount"))
+    topic_df["Average Channel View Count"] = topic_df["Topic"].map(
+        topic_averages(homepage_df, "viewCount"))
     return topic_df
 
 
-
-def topic_averages(homepage_df,col):
+def topic_averages(homepage_df, col):
     '''
     Find averages of all unique topics based on column (subscriberCount or viewCount primarily)
     '''
     avgs = []
     topics = []
     for i in unique_topics(homepage_df):
-        mean = round(categorize_channels([i],homepage_df)[col].mean())
+        mean = round(categorize_channels([i], homepage_df)[col].mean())
         avgs.append(mean)
         topics.append(i)
     dict1 = dict(zip(topics, avgs))
     return dict1
-
